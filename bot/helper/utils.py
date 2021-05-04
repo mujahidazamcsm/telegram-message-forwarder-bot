b02a7e796bf02ea3f00a13304287e884e5e10b4f
mujahidazamcsm <81164002+mujahidazamcsm@users.noreply.github.com>
2021-05-04 23:32:19 +0530

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -2,14 +2,21 @@ import sys
 
 def get_formatted_chats(chats, app):
     formatted_chats = []
+      LOG.info("get_formatted_chats")
+        LOG.info(chats)
+        LOG.info(app)
     for chat in chats:
+       LOG.info("get_formatted_chats for") 
       try:
         if isInt(chat):
+             LOG.info("get_formatted_chats if")
           formatted_chats.append(int(chat))
         elif chat.startswith("@"):
+            LOG.info("get_formatted_chats elif")
           formatted_chats.append(app.get_chat(chat.replace("@", "")).id)
         elif chat.startswith("https://t.me/c/") or chat.startswith("https://telegram.org/c/") or chat.startswith("https://telegram.dog/c/"):
-          chat_id = chat.split("/")[4]
+        LOG.info("get_formatted_chats elif2") 
+        chat_id = chat.split("/")[4]
           if isInt(chat_id):
             chat_id = "-100" + str(chat_id)
             chat_id = int(chat_id)
@@ -22,8 +29,12 @@ def get_formatted_chats(chats, app):
     return formatted_chats
 
 def get_formatted_chat(chat, app):
+    LOG.info("get_formatted_chat")
+      LOG.info(chats)
+        LOG.info(app)
     try:
       if isInt(chat):
+         LOG.info("get_formatted_chat if")
         return int(chat)
       elif chat.startswith("@"):
         return app.get_chat(chat.replace("@", "")).id
